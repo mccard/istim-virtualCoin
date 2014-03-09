dc24bfa60d6ee3d4448fb6e9f10ee173c9b5112e
Macario <franciscomacario@outlook.com>
2014-03-09 03:32:06 -0300

    consumo da API restful Virtual Coin hospedada no jitsu, uso do angularJS

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,14 +1,70 @@
 var coinPage = angular.module('coin-page', []);
-
+var url_default = 'http://virtual-coin-api.jit.su/coin';
 function mainController($scope, $http) {
 	$scope.formData = {};
-	$scope.coins = "unknown";
-	$http.get('http://virtual-coin-api.jit.su/coin')
+	$http.get(url_default+'/')
 		.success(function(data) {
 			$scope.coins = data;
 		})
 		.error(function(data) {
 			console.log('Error: ' + data);
 		});
+
+	$scope.createCoin = function() {
+		$http.post(url_default+'/', $scope.formData)
+			.success(function(data) {
+				$scope.formData = {};
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+	$scope.deleteCoin = function(userId) {
+		var url = url_default+'?userId='+userId;
+		$http.delete(url)
+			.success(function(data) {
+				console.log(data);
+			})
+			.error(function(data) {
+				console.log('Error: ' + data);
+			});
+	};
+
+	$scope.updateCoin = function(userId) {
+		var url = url_default+'?userId='+userId;
+		if ($scope.formData.newUser) url +='&newUser='+$scope.formData.newUser;
+		if ($scope.formData.cash) url +='&cash='+$scope.formData.cash;
+		$http.put(url)
+			.success(function(data){
+				$scope.formData = {};
+			})
+			.error(function(data){
+				console.log('Error: ' + data);
+			});
+	};
+
+	$scope.debitCoin = function(userId) {
+		var url = url_default+'/debit?userId='+userId+'&cash='+$scope.formData.cash;
+		$http.post(url)
+			.success(function(data){
+				$scope.formData = {};
+			})
+			.error(function(data){
+				console.log('Error: ' + data);
+			});
+	};
+
+	$scope.creditCoin = function(userId) {
+		var url = url_default+'/credit?userId='+userId+'&cash='+$scope.formData.cash;
+		$http.post(url)
+			.success(function(data){
+				$scope.formData = {};
+			})
+			.error(function(data){
+				console.log('Error: ' + data);
+			});
+	};
 
 }
